371a514eeff23a7a3d73a3d394a7efbb5638ed04
xavi-mat <xaviermatoses@gmail.com>
2019-02-27 00:41:38 +0100

    Create simple_example.py

---
+++ simple_example.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import myrtf
+
+r = myrtf.Rtf("My Document Title", verbose=True)
+
+r.author = "Myself"
+r.set_layout("A4")
+
+r.par("This text starts a paragraph.")
+r.text(" This text continues the paragraph, note the space before 'This'.")
+r.note("The text of a footnote.", anchor="*")
+
+r.par("A new paragraph begins. Former note and paragraph are automatically closed.")
+r.note("This is the text of the second footnote.")
+r.text(" I'm adding text to the second footnote.")
+r.close_note()
+r.text(" Now I'm adding text to the second paragraph. I had to close the note manually.")
+
+r.create("File name")
